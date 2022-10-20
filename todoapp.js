2003264b028cb87130a55f3a6927d0703e8f43fe
Mikha-il Allie <mikhailallie0@gmail.com>
2022-10-20 16:51:44 +0200

    Save todo
    I have now used js to save a todo. Removal of console log in final stage too. I have also added an alert to the user when the todo already exists or is duplicaated.

--- todoapp.js
+++ todoapp.js
@@ -1,10 +1,43 @@
 // SELECT ELEMENTS
 const form = document.getElementById('todoform');
+const todoInput = document.getElementById('newtodo')
+
+// VARS
+let todos = [];
 
 // FORM SUBMIT
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
+   saveTodo();
 
 });
+
+// SAVE TODO
+function saveTodo(){
+    const todoValue = todoInput.value
+
+    // check if the todoo is empty
+    const isEmpty = todoValue === '';
+
+    // check for duplicate todos
+     const isDuplicate = todos.some((todo) => todo.value.toUpperCase() === todoValue.toUpperCase());
+
+    if(isEmpty){
+        alert("Todo's input is empty");
+
+    } else if (isDuplicate){
+        alert('Todo already exists!');
+    } else {
+        todos.push({
+            value : todoValue,
+            checked : false,
+            color : "#" + Math.floor(Math.random()*16777215).toString(16)
+        });
+        todoInput.value = '';
+
+    }
+}
+
+
 
